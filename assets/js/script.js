c3687a16b3f96d9b6f0215cf6af42b2396afd604
Tom Holohan <thomasholohan8@gmail.com>
2021-06-13 12:29:10 +0000

    Add correct and wrong classes to answer buttons to display on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,12 +27,7 @@ nextButton.addEventListener('click', () => {
 })
 
 function checkAnswer(){
-    if (selectedButton.innerText === shuffledQuestions[currentQuestionIndex].correct.text){
-        incrementScore();
-        console.log('correct answer')
-    } else {
-        
-    }
+    
 }
 
 
@@ -49,6 +44,9 @@ function showQuestion(question){
         let button = document.createElement('button')
         button.innerText = answer.text
         button.classList.add('btn')
+        if (answer.correct){
+            button.dataset.correct = answer.correct;
+        }
         answerButtonsElement.appendChild(button)
         button.addEventListener('click', selectedAnswer)
     })
@@ -60,17 +58,32 @@ function nextQuestion(){
 
 
 
-function selectedAnswer(event){
-    nextButton.classList.remove('hide')
-    selectedButton = event.target;
-    incrementScore();
+function selectedAnswer(e){
+    nextButton.classList.remove('hide');
+    let selectedButton = e.target;
+    let correct = selectedButton.dataset.correct;
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct)
+    })
+    console.log('Your current score is ' + score);
+}
+
+function setStatusClass(element, correct){
+    clearStatusClass(element)
+    if(correct){
+        element.classList.add('correct')
+        score++;
+    } else {
+        element.classList.add('wrong')
+    }
+}
+
+function clearStatusClass(element){
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
 }
 
 let score = 0;
-function incrementScore(){
-    score++;
-    console.log('Your current score is ' + score);
-}
 
 function gameEnd(event){
     let endScreen = document.createElement('h2');
@@ -90,202 +103,182 @@ let questions = [
     {
         question: 'Who is the Yankees current general manager?',
         answers: [
-            {text: 'Thad Levine'},
-            {text: 'Scott Harris'},
-            {text: 'Brian Cashman'},
-            {text: 'Alex Anthopoulos'},
-        ],
-        correct: 'Brian Cashman'
+            {text: 'Thad Levine', correct: false},
+            {text: 'Scott Harris', correct: false},
+            {text: 'Brian Cashman', correct: true},
+            {text: 'Alex Anthopoulos', correct:false},
+        ]
     },
     {
         question: 'Which of these teams does not play in the AL West division?',
         answers: [
-            {text: 'Houston Astros'},
-            {text: 'Seattle Mariners'},
-            {text: 'Texas Rangers'},
-            {text: 'Minnesota Twins'},
-        ],
-        correct: 'Minnesota Twins'
+            {text: 'Houston Astros', correct: false},
+            {text: 'Seattle Mariners', correct: false},
+            {text: 'Texas Rangers', correct: false},
+            {text: 'Minnesota Twins', correct: true},
+        ]
     },
     {
         question: 'What team did Babe Ruth play for first?',
         answers: [
-            {text: 'Boston Red Sox'},
-            {text: 'Chicago White Sox'},
-            {text: 'Baltimore Orioles'},
-            {text: 'New York Yankees'},
-        ],
-        correct: 'Baltimore Orioles'
+            {text: 'Boston Red Sox', correct: false},
+            {text: 'Chicago White Sox', correct: false},
+            {text: 'Baltimore Orioles', correct: true},
+            {text: 'New York Yankees', correct: false},
+        ]
     },
     {
         question: 'What team did Babe Ruth play for first?',
         answers: [
-            {text: 'Boston Red Sox'},
-            {text: 'Chicago White Sox'},
-            {text: 'Baltimore Orioles'},
-            {text: 'New York Yankees'},
-        ],
-        correct: 'Boston Red Sox'
+            {text: 'Boston Red Sox', correct: true},
+            {text: 'Chicago White Sox', correct: false},
+            {text: 'Baltimore Orioles', correct: false},
+            {text: 'New York Yankees', correct: false},
+        ]
     },
     {
         question: 'What team did Shohei Ohtani play for in the Japanese league?',
         answers: [
-            {text: 'Hokkaido Nippon-Ham Fighters'},
-            {text: 'Hanshin Tigers'},
-            {text: 'Yomiuri Giants'},
-            {text: 'Orix Buffaloes'},
-        ],
-        correct: 'Hokkaido Nippon-Ham Fighters'
+            {text: 'Hokkaido Nippon-Ham Fighters', correct: true},
+            {text: 'Hanshin Tigers', correct: false},
+            {text: 'Yomiuri Giants', correct: false},
+            {text: 'Orix Buffaloes', correct: false},
+        ]
     },
     {
         question: 'Which pitcher holds the world record for fastest ball thrown?',
         answers: [
-            {text: 'Jordan Hicks'},
-            {text: 'Aroldis Chapman'},
-            {text: 'Randy Johnson'},
-            {text: 'Justin Verlander'},
-        ],
-        correct: 'Aroldis Chapman'
+            {text: 'Jordan Hicks', correct: false},
+            {text: 'Aroldis Chapman', correct: true},
+            {text: 'Randy Johnson', correct: false},
+            {text: 'Justin Verlander', correct: false},
+        ]
     },
     {
         question: 'Who did Boston play against in the first World Series?',
         answers: [
-            {text: 'Arizona'},
-            {text: 'Miami'},
-            {text: 'Houston'},
-            {text: 'Pittsburgh'},
-        ],
-        correct: 'Pittsburgh'
+            {text: 'Arizona', correct: false},
+            {text: 'Miami', correct: false},
+            {text: 'Houston', correct: false},
+            {text: 'Pittsburgh', correct: true},
+        ]
     },
     {
         question: 'How long was the shortest game in baseball history?',
         answers: [
-            {text: '1 hour'},
-            {text: '2 hours'},
-            {text: '51 minutes'},
-            {text: '22 minutes'},
-        ],
-        correct: '51 minutes'
+            {text: '1 hour', correct: false},
+            {text: '2 hours', correct: false},
+            {text: '51 minutes', correct: true},
+            {text: '22 minutes', correct: false},
+        ]
     },
     {
         question: 'What year was the MLB founded?',
         answers: [
-            {text: '1876'},
-            {text: '1932'},
-            {text: '1903'},
-            {text: '1901'},
-        ],
-        correct: '1903'
+            {text: '1876', correct: false},
+            {text: '1932', correct: false},
+            {text: '1903', correct: true},
+            {text: '1901', correct: false},
+        ]
     },
     {
         question: 'How long was the longest game in baseball history?',
         answers: [
-            {text: '8 hours 6 minutes'},
-            {text: '4 hours 3 minutes'},
-            {text: '9 days'},
-            {text: '13 hours 20 minutes'},
-        ],
-        correct: '8 hours 6 minutes'
+            {text: '8 hours 6 minutes', correct: true},
+            {text: '4 hours 3 minutes', correct: false},
+            {text: '9 days', correct: false},
+            {text: '13 hours 20 minutes', correct: false},
+        ]
     },
     {
         question: 'What player is credited with the most home runs in their career?',
         answers: [
-            {text: 'Albert Pujols'},
-            {text: 'Willie Mays'},
-            {text: 'Barry Bonds'},
-            {text: 'Ken Griffey Jr.'},
-        ],
-        correct: 'Barry Bonds'
+            {text: 'Albert Pujols', correct: false},
+            {text: 'Willie Mays', correct: false},
+            {text: 'Barry Bonds', correct: true},
+            {text: 'Ken Griffey Jr.', correct: false},
+        ]
     },
     {
         question: 'Which player has played for the most franchises in MLB history?',
         answers: [
-            {text: 'Ron Villione'},
-            {text: 'Edwin Jackson'},
-            {text: 'Deacon McGuire'},
-            {text: 'Henry Blanco'},
-        ],
-        correct: 'Edwin Jackson'
+            {text: 'Ron Villione', correct: false},
+            {text: 'Edwin Jackson', correct: true},
+            {text: 'Deacon McGuire', correct: false},
+            {text: 'Henry Blanco', correct: false},
+        ]
     },
     {
         question: 'What team was caught cheating by stealing signs in 2017 and 2018?',
         answers: [
-            {text: 'Baltimore Orioles'},
-            {text: 'Oakland Athletics'},
-            {text: 'Philadelphia Phillies'},
-            {text: 'Houston Astros'},
-        ],
-        correct: 'Houston Astros'
+            {text: 'Baltimore Orioles', correct: false},
+            {text: 'Oakland Athletics', correct: false},
+            {text: 'Philadelphia Phillies', correct: false},
+            {text: 'Houston Astros', correct: true},
+        ]
     },
     {
         question: 'Where is the baseball Hall Of Fame?',
         answers: [
-            {text: 'Cooperstown, New York'},
-            {text: 'Dublin, Georgia'},
-            {text: 'Los Angeles, California'},
-            {text: 'Seattle, Washington'},
-        ],
-        correct: 'Cooperstown, New York'
+            {text: 'Cooperstown, New York', correct: true},
+            {text: 'Dublin, Georgia', correct: false},
+            {text: 'Los Angeles, California', correct: false},
+            {text: 'Seattle, Washington', correct: false},
+        ]
     },
     {
         question: 'Who is the oldest recorded player in the MLB?',
         answers: [
-            {text: "Charley O'Leary"},
-            {text: 'Leroy Paige'},
-            {text: "Jim O'Rourke"},
-            {text: 'Hoyt Wilhelm'},
-        ],
-        correct: 'Leroy Paige'
+            {text: "Charley O'Leary", correct: false},
+            {text: 'Leroy Paige', correct: true},
+            {text: "Jim O'Rourke", correct: false},
+            {text: 'Hoyt Wilhelm', correct: false},
+        ]
     },
     {
         question: 'What famous slugger retired after suffering from ALS?',
         answers: [
-            {text: 'Red Ruffing'},
-            {text: 'Johnny Murphy'},
-            {text: 'Lou Gehrig'},
-            {text: 'Joe DiMaggio'},
-        ],
-        correct: 'Lou Gehrig'
+            {text: 'Red Ruffing', correct: false},
+            {text: 'Johnny Murphy', correct: false},
+            {text: 'Lou Gehrig', correct: true},
+            {text: 'Joe DiMaggio', correct: false},
+        ]
     },
     {
         question: 'How many players are in a teams batting order?',
         answers: [
-            {text: '12'},
-            {text: '9'},
-            {text: '8'},
-            {text: '6'},
-        ],
-        correct: '9'
+            {text: '12', correct: false},
+            {text: '9', correct: true},
+            {text: '8', correct: false},
+            {text: '6', correct: false},
+        ]
     },
     {
         question: 'Which of these is not a type of pitch?',
         answers: [
-            {text: 'Shoulder Curveball'},
-            {text: 'Slider'},
-            {text: 'Eephus'},
-            {text: 'Four-Seam Fastball'},
-        ],
-        correct: 'Shoulder Curveball'
+            {text: 'Shoulder Curveball', correct: true},
+            {text: 'Slider', correct: false},
+            {text: 'Eephus', correct: false},
+            {text: 'Four-Seam Fastball', correct: false},
+        ]
     },
     {
         question: 'How many balls does a pitcher need to throw to walk a batter?',
         answers: [
-            {text: '3'},
-            {text: '5'},
-            {text: '2'},
-            {text: '4'},
-        ],
-        correct: '4'
+            {text: '3', correct: false},
+            {text: '5', correct: false},
+            {text: '2', correct: false},
+            {text: '4', correct: true},
+        ]
     },
     {
         question: 'What is another name for baseball stadium?',
         answers: [
-            {text: 'A Ring'},
-            {text: 'An Octagon'},
-            {text: 'A Diamond'},
-            {text: 'An Alley'},
-        ],
-        correct: 'A Diamond',
+            {text: 'A Ring', correct: false},
+            {text: 'An Octagon', correct: false},
+            {text: 'A Diamond', correct: true},
+            {text: 'An Alley', correct: false},
+        ]
     }
 ]
 
